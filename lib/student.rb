946cf95c80bffd7bdd33e7942358251d96d29dbe
Lizzie Chan <lchan217@gmail.com>
2019-03-26 19:13:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
       LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
-      self.find_by_name(row)
+      self.new_from_db(row)
     end.first
   end
 # def self.find_by_name(name)
