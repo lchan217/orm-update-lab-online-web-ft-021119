84b0e64271b931b3689aff6c340d190d7c64146d
Lizzie Chan <lchan217@gmail.com>
2019-03-26 18:53:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
       SQL
       DB[:conn].execute(sql, self.name, self.grade, self.id)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    end
+    
   end 
   def update
    sql = "UPDATE songs SET name = ?, grade = ? WHERE id = ?"
