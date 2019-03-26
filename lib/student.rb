77baf464c1612fc8b80f8c0dfe0b36a88a5a33aa
Lizzie Chan <lchan217@gmail.com>
2019-03-26 19:10:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,11 @@ class Student
       student
   end 
   def self.find_by_name(name)
-    
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE name = ? 
+    SQL
+    DB[:conn].execute(sql, name)
   end
 end
