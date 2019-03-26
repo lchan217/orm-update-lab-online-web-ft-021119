16a3799db70326d0fa0229ebaf7dc5ee8600aa84
Lizzie Chan <lchan217@gmail.com>
2019-03-26 19:04:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,4 +51,12 @@ class Student
 #   new_student.grade = row[2] 
 #   new_student
 # end 
+  def self.new_from_db(row) #[1, "Pat", 12]
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
 end
