a5904b0c2ff3417808d5a58846d5f6a5c1350da5
Lizzie Chan <lchan217@gmail.com>
2019-03-26 18:55:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,15 +23,7 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end 
-  # def save 
-  #     sql = <<-SQL
-  #       INSERT INTO students
-  #       VALUES (?,?)
-  #     SQL
-  #     DB[:conn].execute(sql, self.name, self.grade)
-  #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  # end 
-   def save
+  def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?,?)
